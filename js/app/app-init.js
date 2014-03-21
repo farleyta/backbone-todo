17aa89b4be1ab1001291f73c5dde2bb5637a0b6b
Tim Farley <me@timfarley.com>
2014-03-21 19:20:20 -0400

    Some events added to App View.

--- js/app/app-init.js
+++ js/app/app-init.js
@@ -1,3 +1,9 @@
 toDoApp.todoList = new toDoApp.ToDoList();
 
 toDoApp.todoView = new toDoApp.AppView();
+
+// toDoApp.todoList.reset([
+//     { title: "Test2", isComplete: true }, 
+//     { title: "Test3", isComplete: false }, 
+//     { title: "Test4", isComplete: false }
+// ]);
--- js/app/views/app.js
+++ js/app/views/app.js
@@ -2,37 +2,81 @@ var toDoApp = toDoApp || {};
 
 toDoApp.AppView = Backbone.View.extend({
 
-	// This element already exists as a skeleton in index.html
-	el: '#todoapp',
+    // This element already exists as a skeleton in index.html
+    el: '#todoapp',
 
-	// For the stats at the bottom of the page
-	statsTemplate: _.template( $('#stats-template').html() ),
+    // For the stats at the bottom of the page
+    statsTemplate: _.template( $('#stats-template').html() ),
 
-	// On init, we bind to the relevant events on the ToDoList collection, 
-	// whenever items are added or changed
-	initialize: function(){
-		// store references to DOM element objects
-		this.$allCheckbox = this.$('#toggle-all');
-		this.$input = this.$('#new-todo');
-		this.$footer = this.$('#footer');
-		this.$main = this.$('#main');
+    // On init, we bind to the relevant events on the ToDoList collection, 
+    // whenever items are added or changed
+    initialize: function(){
 
-		this.listenTo(toDoApp.todoList, 'add', this.addToDo);
-		this.listenTo(toDoApp.todoList, 'reset', this.addAllToDos);
-	},
+        // Shortcut to access our Collection
+        this.todoList = toDoApp.todoList;
 
-	addToDo: function( todo ){
-		// Create a new single ToDo view, referencing the ToDo Model that is being added
-		var toDoView = new toDoApp.ToDoView({ model: todo });
-		// Append the rendered element to the #todo-list <ul>
-		$('#todo-list').append( toDoView.render().el );
-	},
+        // store references to DOM element objects
+        this.$allCheckbox = this.$('#toggle-all');
+        this.$input = this.$('#new-todo');
+        this.$footer = this.$('#footer');
+        this.$main = this.$('#main');
 
-	addAllToDos: function( allToDos ) {
-		// Reset the HTML for the ToDo List
-		this.$('#todo-list').html('');
-		// iterate through all ToDo items run the addToDo function from above
-		allToDos.each( this.addToDo, this );
-	}
+        this.listenTo(this.todoList, 'add', this.addToDo);
+        this.listenTo(this.todoList, 'reset', this.addAllToDos);
+    },
+
+    // Delegate certain events for creating new items and clearing old ones
+    events: {
+        'keypress #new-todo': 'createOnEnter',
+        'click #clear-completed': 'clearCompleted'
+    },
+
+    addToDo: function( todo ){
+        // Create a new single ToDo view, referencing the ToDo Model that is being added
+        // var toDoView = new toDoApp.ToDoView({ model: todo });
+        // Append the rendered element to the #todo-list <ul>
+        // $('#todo-list').append( toDoView.render().el );
+        console.log( todo.get('title'));
+    },
+
+    addAllToDos: function( allToDos ) {
+        // Reset the HTML for the ToDo List
+        this.$('#todo-list').html('');
+        // iterate through all ToDo items run the addToDo function from above
+        allToDos.each( this.addToDo, this );
+    },
+
+    clearCompleted: function() {
+        _.invoke( this.todoList.getCompleted(), function() {
+            this.destroy({
+                success: function(model){
+                    console.log('Model: ' + model.get('orderNum') + ' successfully destroyed.');
+                },
+                error: function(model, response){
+                    console.log('Error: ' + response + '\nModel: ' + model.get('orderNum') + ' successfully destroyed.');
+                }
+            });
+        });
+    },
+
+    createOnEnter: function( e ) {
+        //if the keypress is anything other than Enter (13), or the field is empty
+        if ( e.which !== 13 || !this.$input.val().trim() ) {
+            return;
+        } else {
+            this.todoList.create( this.newToDoAttrs() );
+        }
+        // And clear the input for the next task
+        this.$input.val('');
+
+    },
+
+    newToDoAttrs: function() {
+        return {
+            title: this.$input.val(),
+            orderNum: this.todoList.nextOrderNum(),
+            isComplete: false
+        };
+    }
 
 });
