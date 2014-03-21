58e5d3a9fa9f81ebe36178e8c7cddced89437992
Tim Farley <me@timfarley.com>
2014-03-21 17:40:16 -0400

    Added top level App View.

--- js/app/app.js
+++ js/app/app.js
@@ -15,7 +15,7 @@ toDoApp.ToDo = Backbone.Model.extend({
 // Create a new wrapper object
 var toDoApp = toDoApp || {};
 
-var ToDoList = Backbone.Collection.extend({
+toDoApp.ToDoList = Backbone.Collection.extend({
     
     model: toDoApp.ToDo,
 
@@ -64,17 +64,36 @@ toDoApp.AppView = Backbone.View.extend({
 	// For the stats at the bottom of the page
 	statsTemplate: _.template( $('#stats-template').html() ),
 
-	// On init, we bind to the relevant events on the ToDos collection, whenever
-	// items are added or changed
+	// On init, we bind to the relevant events on the ToDoList collection, 
+	// whenever items are added or changed
 	initialize: function(){
-		// store DOM element objects
+		// store references to DOM element objects
 		this.$allCheckbox = this.$('#toggle-all');
 		this.$input = this.$('#new-todo');
 		this.$footer = this.$('#footer');
 		this.$main = this.$('#main');
+
+		this.listenTo(toDoApp.todoList, 'add', this.addToDo);
+		this.listenTo(toDoApp.todoList, 'reset', this.addAllToDos);
 	},
+
+	addToDo: function( todo ){
+		// Create a new single ToDo view, referencing the ToDo Model that is being added
+		var toDoView = new toDoApp.ToDoView({ model: todo });
+		// Append the rendered element to the #todo-list <ul>
+		$('#todo-list').append( toDoView.render().el );
+	},
+
+	addAllToDos: function( allToDos ) {
+		// Reset the HTML for the ToDo List
+		this.$('#todo-list').html('');
+		// iterate through all ToDo items run the addToDo function from above
+		allToDos.each( this.addToDo, this );
+	}
 
 });
 
 
-toDoApp.todoList = new ToDoList();
+toDoApp.todoList = new toDoApp.ToDoList();
+
+toDoApp.todoView = new toDoApp.AppView();
